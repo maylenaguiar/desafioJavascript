3d8db541bc1b19c7144d7b4ddbb1a8fd2cb3171b
maylen aguiar <maylenaguiar.psi@gmail.com>
2021-09-01 19:03:37 -0300

    desafio

---
+++ main.js
@@ -0,0 +1,16 @@
+
+/*Desafio*/
+let nombre = prompt ("Ingresa tu nombre")
+let apellido = prompt("Ingresa tu apellido");
+let nombreCompleto = ("Hola " + nombre + " " + apellido)
+alert(nombreCompleto);
+
+let edad = prompt("Ingresa tu edad");
+let años = parseInt(edad);
+
+let numero = 20;
+
+let resultado = (años + numero);
+ 
+console.log (resultado);
+
